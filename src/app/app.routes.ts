a5f8b3a148e245e0a2de692568da699cb4c6d569
LorenzoRossini01 <lory.rox.01@gmail.com>
2025-10-02 18:48:31 +0200

    feat: add blog route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Services } from './pages/services/services';
 import { ServicesDetail } from './pages/services-detail/services-detail';
 import { ServiceInfos } from './pages/service-infos/service-infos';
 import { Contacts } from './pages/contacts/contacts';
+import { Blog } from './pages/blog/blog';
 
 export const routes: Routes = [
   {
@@ -27,6 +28,10 @@ export const routes: Routes = [
   {
     path: 'servizi-offerti/:serviceSlug/:infoServiceSlug',
     component: ServiceInfos,
+  },
+  {
+    path: 'blog',
+    component: Blog,
   },
   {
     path: 'contatti',
