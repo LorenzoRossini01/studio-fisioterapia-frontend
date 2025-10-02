2cc8a5bc44900330587d384e5847170701fd01c8
LorenzoRossini01 <lory.rox.01@gmail.com>
2025-10-02 16:41:37 +0200

    feat: initialization project

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,12 +1,13 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { provideServerRendering, withAppShell, withRoutes } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { App } from './app';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(withRoutes(serverRoutes))
-  ]
+    provideServerRendering(withRoutes(serverRoutes), withAppShell(App)),
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
